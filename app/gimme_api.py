aae25449bf62e46eea116c0a42c2c5cb013d3fc7
mazy06000 <mazymohamed06@gmail.com>
2023-01-17 23:44:02 +0000

    FastAPI added to the backend

---
+++ app/gimme_api.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, HTTPException
+from gimme import generate_branding_snippet, generate_keywords, validate_lenght
+
+MAX_INPUT_LENGTH = 32
+app = FastAPI()
+
+
+@app.get("/generate_snippet")
+async def generate_snippet_api(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet, "keywords": []}
+
+
+@app.get("/generate_keywords")
+async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": None, "keywords": keywords}
+
+
+@app.get("/generate_snippet_keywords")
+async def generate_snippet_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
+
+
+def validate_input_length(prompt: str):
+    if not validate_lenght(prompt):
+        raise HTTPException(status_code=400,
+                            detail=f"Input length is too long. Must be under {MAX_INPUT_LENGTH}.")
+# uvicorn gimme_api:app --reload
