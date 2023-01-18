085bcce20743ee51be64b764842390fcca60bde2
mazy06000 <mazymohamed06@gmail.com>
2023-01-18 20:34:47 +0000

    lambda function added

--- gimme-infra/lib/gimme-infra-stack.ts
+++ gimme-infra/lib/gimme-infra-stack.ts
@@ -1,13 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 export class GimmeInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const layer = new lambda.LayerVersion(this, "BaseLayer", {
-      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      code: lambda.Code.fromAsset("lambda-base-layer/layer.zip"),
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_8]
     })
     const apilambda = new lambda.Function(this, "ApiFunction", {
@@ -15,6 +18,9 @@ export class GimmeInfraStack extends cdk.Stack {
       code: lambda.Code.fromAsset("../app/"),
       handler: "gimme_api.handler",
       layers: [layer],
+      environment: {
+        "OPENAI_API_KEY": process.env.OPENAI_API_KEY ?? "",
+      }
     })
   }
 }
